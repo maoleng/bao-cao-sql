f6309c990ca2d948201dd787ef5970d2ae6c75d6
Bùi Hữu Lộc <feature451@gmail.com>
2022-11-23 19:55:50 +0700

    reformat: autload class

--- Code/Source/query_2.php
+++ Code/Source/query_2.php
@@ -5,7 +5,7 @@
 
 
 <?php
-require 'vendor/autoload.php';
+require 'AutoLoad.php';
 
 const CONDITIONALS = [
 	'id' => null,
@@ -32,38 +32,4 @@ if (! empty($_FILES)) {
         $result[] = $result_row;
     }
     dd($result);
-}
-
-function positionOfCondition($records): array
-{
-    $columns = explode(',', $records[0]);
-    $columns = array_map(static function ($column) {
-        if (str_starts_with($column, '__')) {
-            return substr($column, 2);
-        }
-        if (str_starts_with($column, '_')) {
-            return substr($column, 1);
-        }
-
-        return $column;
-	}, $columns);
-    $pos = [];
-    $conditional_column = array_keys(CONDITIONALS);
-    foreach ($conditional_column as $column) {
-		$pos[] = array_search($column, $columns, true);
-	}
-
-    return $pos;
-}
-
-function getRecords($file): array
-{
-	$file_content = file_get_contents($file);
-	preg_match_all('/.*\r/', $file_content, $matches);
-    $records = [];
-    foreach ($matches[0] as $record) {
-        $records[] = str_replace('\,', '', substr($record, 0, -1));
-    }
-
-	return $records;
 }
